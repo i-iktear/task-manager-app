cbfa49269f26379c9392a17bb29c41212e9e7c16
Iktear Uddin <emon5aurk@gmail.com>
2022-07-30 14:18:31 +0600

    Added: date to tasks

--- src/data/tasks.js
+++ src/data/tasks.js
@@ -4,11 +4,13 @@ export const tasks = [
     title: "Task 1",
     description: "Task 1 description",
     assignTo: "user1",
+    date: new Date(),
   },
   {
     id: "202",
     title: "Task 2",
     description: "Task 2 description",
     assignTo: "user2",
+    date: new Date(),
   },
 ];
