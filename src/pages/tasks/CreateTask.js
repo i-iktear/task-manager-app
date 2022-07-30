92d8ab4adb22d236405d6b6cafd04e571a513224
Iktear Uddin <emon5aurk@gmail.com>
2022-07-30 13:46:46 +0600

    added: tasks homepage & create task & update task

---
+++ src/pages/tasks/CreateTask.js
@@ -0,0 +1,135 @@
+import React, { useEffect, useState } from "react";
+import { Container, Form, Button, Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { v4 as uuid } from "uuid";
+import { useParams } from "react-router-dom";
+
+// import Loader from "../components/Loader";
+// import Toaster from "../components/Toaster";
+
+const CreateTask = ({ match }) => {
+  let navigate = useNavigate();
+  let { id } = useParams();
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [assigne, setAssigne] = useState("");
+  const [membersList, setMembersList] = useState([]);
+
+  useEffect(() => {
+    if (id) {
+      const membersListFromStorage = localStorage.getItem("tasksList")
+        ? JSON.parse(localStorage.getItem("tasksList"))
+        : null;
+      const member = membersListFromStorage.find((member) => member.id === id);
+      setTitle(member?.title);
+      setDescription(member?.description);
+      setAssigne(member?.assignTo);
+    }
+    const membersListFromStorage = localStorage.getItem("membersList")
+      ? JSON.parse(localStorage.getItem("membersList"))
+      : null;
+    setMembersList(membersListFromStorage);
+  }, [id]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const tasksListFromStorage = localStorage.getItem("tasksList")
+      ? JSON.parse(localStorage.getItem("tasksList"))
+      : null;
+
+    if (id) {
+      const newTaskList = tasksListFromStorage.map((task) => {
+        if (task.id === id) {
+          task.title = title;
+          task.description = description;
+          task.assignTo = assigne;
+        }
+        return task;
+      });
+      localStorage.setItem("tasksList", JSON.stringify(newTaskList));
+      navigate("/tasks");
+    } else {
+      localStorage.setItem(
+        "tasksList",
+        JSON.stringify([
+          ...tasksListFromStorage,
+          {
+            id: uuid(),
+            title,
+            description,
+            assignTo: assigne,
+          },
+        ])
+      );
+      navigate("/tasks");
+    }
+  };
+
+  return (
+    <Container>
+      {/* {error && <Toaster variant="danger">{error}</Toaster>}
+      {loading ? <Loader /> : null} */}
+      <Row className="justify-content-md-center">
+        <Col xs={12} md={6}>
+          <center>
+            <h1> {id ? "Edit Task" : "Create Task"} </h1>
+          </center>
+          {/* {error && <Message variant="danger">{error}</Message>}
+          {loading && <Loader />} */}
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="title">
+              <Form.Label>
+                {" "}
+                <b>Title *</b>{" "}
+              </Form.Label>
+              <Form.Control
+                placeholder="Enter Title"
+                value={title}
+                required
+                onChange={(e) => setTitle(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="description">
+              <Form.Label>
+                <b>Descripion</b>{" "}
+              </Form.Label>
+              <Form.Control
+                placeholder="Enter Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="category">
+              <Form.Label>Assign To</Form.Label>
+              <Form.Control
+                as="select"
+                value={assigne}
+                onChange={(e) => setAssigne(e.target.value)}
+              >
+                {/* <option value="">Choose...</option>
+                <option value="Web App">Web App</option>
+                <option value="Android App">Android App</option>
+                <option value="Ios App">Ios App</option> */}
+                {membersList &&
+                  membersList.map((member) => (
+                    <option key={member.id} value={member.name}>
+                      {member.name}
+                    </option>
+                  ))}
+              </Form.Control>
+            </Form.Group>
+            <br />
+            <Button type="submit" size="sm" variant="primary">
+              {id ? "Update Task" : "Create Task"}
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default CreateTask;
---
+++ src/pages/tasks/TasksHomepage.js
@@ -0,0 +1,92 @@
+import React, { useEffect } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Button, Table } from "react-bootstrap";
+import Loader from "../../components/Loader";
+import { listTasks, deleteTaskAction } from "../../redux/actions/taskActions";
+import { useDispatch, useSelector } from "react-redux";
+
+const TasksHomepage = ({ history }) => {
+  // const [isLoading, setIsLoading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  const getTaskList = useSelector((state) => state.getTaskList);
+  const { loading, tasks } = getTaskList;
+
+  const deleteTask = useSelector((state) => state.deleteTask);
+  const { loading: deleteLoading, success } = deleteTask;
+
+  useEffect(() => {
+    dispatch(listTasks());
+  }, [dispatch, success]);
+
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(deleteTaskAction(id));
+    }
+  };
+
+  return (
+    <>
+      {loading || deleteLoading ? <Loader /> : null}
+      <center>
+        <b>Tasks List</b> <i className="far fa-list-alt"></i>
+      </center>
+      <LinkContainer to={`/add/task`}>
+        <Button className="ml-auto" variant="primary">
+          Create Task
+        </Button>
+      </LinkContainer>
+      <br />
+      <br />
+      {tasks?.length > 0 && (
+        <>
+          <Table striped hover resposnsive="true" className="table-sm">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Assigned To</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {tasks.map((task) => (
+                <tr key={task?.id}>
+                  <td>
+                    <LinkContainer to={`/update/task/${task.id}`}>
+                      <div>{task?.title}</div>
+                    </LinkContainer>
+                  </td>
+                  <td>{task?.description}</td>
+                  <td>{task?.assignTo}</td>
+                  <td>
+                    <LinkContainer to={`/update/task/${task.id}`}>
+                      <Button variant="info" className="btn-sm">
+                        <i className="fas fa-edit"></i>
+                      </Button>
+                    </LinkContainer>
+                  </td>
+                  <td>
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={() => {
+                        deleteHandler(task.id);
+                      }}
+                    >
+                      <i className="fas fa-trash"></i>
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
+    </>
+  );
+};
+
+export default TasksHomepage;
